1ca07fe6a673d865b9781a9bb8bdfa76a5410c10
ghalibhani <ghalibpratama.gpro@gmail.com>
2022-11-10 14:32:59 +0700

    Initialize movie catalogue automated testing implemented

---
+++ codecept.conf.js
@@ -0,0 +1,36 @@
+const { setHeadlessWhen } = require('@codeceptjs/configure');
+const { setCommonPlugins } = require('@codeceptjs/configure');
+
+setCommonPlugins();
+// turn on headless mode when running with HEADLESS=true environment variable
+// HEADLESS=true npx codecept run
+setHeadlessWhen(process.env.HEADLESS);
+
+// enable all common plugins https://github.com/codeceptjs/configure#setcommonplugins
+setCommonPlugins();
+
+exports.config = {
+  tests: 'e2e/**/*.spec.js',
+  output: 'e2e/outputs',
+  helpers: {
+    Puppeteer: {
+      url: 'http://localhost:9000',
+      show: true,
+      windowSize: '1280x720',
+    },
+  },
+  include: {
+    I: './steps_file.js',
+  },
+  bootstrap: null,
+  mocha: {},
+  name: 'movie-catalogue-lite',
+  plugins: {
+    retryFailedStep: {
+      enabled: true,
+    },
+    screenshotOnFail: {
+      enabled: true,
+    },
+  },
+};
